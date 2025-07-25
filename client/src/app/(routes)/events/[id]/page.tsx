7af77efa0ecbe6f33a05e1d6904e5f556aeb0668
ashim086 <magarashim69086@gmail.com>
2025-07-25 17:03:57 +0545

    Event details

--- client/src/app/(routes)/events/[id]/page.tsx
+++ client/src/app/(routes)/events/[id]/page.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 import ArtistSlider from "@/components/features/EventDetails/ArtistSlider";
 import MapSection from "@/components/ui/MapSection";
+import EventDetails from "@/components/features/EventDetails/EvenDetails";
 
 const EventDetailsPage = () => {
   return (
-  <main className="min-h-screen pt-10 bg-gray-50">
- 
-  <div className="w-full px-20 sm:px-6 lg:px-10 max-w-[1440px] mx-auto">
-    <section className="mb-10 text">
-      <h2 className="text-lg sm:text-xl font-semibold mb-1">
-        Get Ready to Meet the Artists!
-      </h2>
-      <h1 className="text-2xl sm:text-3xl text-[#2B7BDB] font-bold">
-        Event Artist
-      </h1>
-    </section>
+    <main className="min-h-screen pt-10 bg-gray-50">
 
-      <ArtistSlider />
-      <MapSection />
-    </div>
-</main>
+      <div className="w-full px-20 sm:px-6 lg:px-10 max-w-[1440px] mx-auto">
+        <section className="mb-10 text">
+          <EventDetails />
+
+          <h2 className="text-lg sm:text-xl font-semibold mb-1">
+            Get Ready to Meet the Artists!
+          </h2>
+          <h1 className="text-2xl sm:text-3xl text-[#2B7BDB] font-bold">
+            Event Artist
+          </h1>
+        </section>
+
+        <ArtistSlider />
+        <MapSection />
+      </div>
+    </main>
   );
 };
 
