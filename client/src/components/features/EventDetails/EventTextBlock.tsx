5dd3e41ef8c28aea1b73a326c9855a679d7117ed
Shubham Shrestha <shrestha.subham.60@gmail.com>
2025-07-25 16:24:08 +0545

    EventTextBlock

---
+++ client/src/components/features/EventDetails/EventTextBlock.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const EventTextBlock = () => {
+  return (
+    <div>
+         <section className="mb-4 text px-20">
+      <h2 className="text-lg sm:text-xl font-semibold mb-1">
+        Get Ready to Meet the Artists!
+      </h2>
+      <h1 className="text-2xl sm:text-3xl text-[#2B7BDB] font-bold">
+        Event Artist
+      </h1>
+    </section>
+    </div>
+  )
+}
+
+export default EventTextBlock
